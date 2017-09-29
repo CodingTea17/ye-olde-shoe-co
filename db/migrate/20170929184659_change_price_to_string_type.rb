8791f34026b0212c2deff021867b05f6bd773174
Dawson Mortenson <dawson.mortenson@gmail.com>
2017-09-29 13:31:15 -0700

    spent way too much time trying to current the money to a currency object using the Money module...Ended up throwing in a '$'...

--- db/migrate/20170929184659_change_price_to_string_type.rb
+++ db/migrate/20170929184659_change_price_to_string_type.rb
@@ -1,8 +1,5 @@
 class ChangePriceToStringType < ActiveRecord::Migration[5.1]
   def change
-    def change
-      remove_column(:brands, :price, :integer)
-      add_column(:brands, :price, :string)
-    end
+    change_column :brands, :price, :string
   end
 end
--- lib/brand.rb
+++ lib/brand.rb
@@ -1,4 +1,11 @@
 class Brand < ActiveRecord::Base
   has_many :inventories
   has_many :stores, through: :inventories
+  validates(:name, {:presence => true, :length => {:maximum => 25}})
+  before_save(:capitalize_brand)
+
+private
+  def capitalize_brand
+    self.name=(name().split.map!{|word| word.capitalize}.join(' '))
+  end
 end
